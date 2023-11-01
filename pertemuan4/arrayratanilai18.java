5a01400cb60d3a307f6d7f8d666a7893bd1ee360
farhanfahraby <144287793+farhanfahraby@users.noreply.github.com>
2023-11-01 16:20:24 +0700

    tugassss tugass tugasss tugassss

--- pertemuan4/arrayratanilai18.java
+++ pertemuan4/arrayratanilai18.java
@@ -1,30 +1,35 @@
 import java.util.Scanner;
-public class arrayratanilai18 {
 
+public class arrayratanilai18 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        double total = 0;
-        double rata2;
-        int[] nilaimhs= new int[5];
 
-        for (int i = 0; i < nilaimhs.length; i++) {
-            System.out.println("masukan nilai mahasiswa ke -" + (i+1) + " : ");
-            nilaimhs [i] = input.nextInt();
-            }
-                
-                if total += nilaimhs.length;
-                if rata2 = total/nilaimhs.length;
-            if (rata2 > 70){
-                System.out.println("mahasiswa lulus" + rata2);
-               
-                } 
-            if (rata2< 70){
-                    System.out.println("mahasiswa tidak lulus " + rata2 );
-                 }
-          
+        System.out.print("Masukkan jumlah nilai mahasiswa: ");
+        int jumlahnilai = input.nextInt();
+
+        int[] nilaiMahasiswa = new int[jumlahnilai];
+
+        for (int i = 0; i < jumlahnilai; i++) {
+            System.out.print("Masukkan nilai mahasiswa ke-" + (i + 1) + ": ");
+            nilaiMahasiswa[i] = input.nextInt();
         }
-        
+
+        double total = 0;
+        for (int nilai : nilaiMahasiswa) {
+            total += nilai;
+        }
+
+        double rata = total / jumlahnilai;
+        System.out.println("Rata-rata nilai = " + rata);
+
+        if (rata >= 70) {
+            System.out.println("Rata-rata nilai lulus = " + rata);
+        } else {
+            System.out.println("Rata-rata nilai tidak lulus = " + rata);
+        }
     }
+}
+
 
          
           
