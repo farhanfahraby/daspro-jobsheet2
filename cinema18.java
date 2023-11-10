5b24b281cadbbc80c7d4af1c3aed50b87ac6602d
farhanfahraby <144287793+farhanfahraby@users.noreply.github.com>
2023-11-10 14:30:57 +0700

    tugass tugass

--- cinema18.java
+++ cinema18.java
@@ -2,6 +2,14 @@ public class cinema18 {
     public static void main(String[] args) {
         String [][] audience = new String[4][2];
         System.out.println(audience.length);
+        audience[0][0] = "Amin";
+        audience[0][1] = "Bena";
+        audience[1][0] = "Candra";
+        audience[1][1] = "Dela";
+        audience[2][0] = "Eka";
+        audience[2][1] = "Farhan";
+        audience[3][0] = "Gisel";
+        audience[3][1] = "Hana";
 
         for (int i = 0; i < audience.length; i++){
             System.out.println("length of row "+ (i + 1) + ": " + audience[i].length);
@@ -14,6 +22,16 @@ public class cinema18 {
 
        for (String [] rowaudience : audience ){
         System.out.println("lenght of row:" + rowaudience.length );
+
+        System.out.println("audiences in the row 3: ");
+        for (int i = 0 ; i < audience[2].length;i++){
+            System.out.println(audience [2][i]);
+
+           
+            
+        }
+        
+        }
        }
     }
-}
+
