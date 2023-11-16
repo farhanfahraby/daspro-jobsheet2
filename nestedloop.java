a9a5249c7e1db48a44c97daafc5b1bd46d65a4d3
farhanfahraby <144287793+farhanfahraby@users.noreply.github.com>
2023-11-16 23:18:03 +0700

    tugasss tugass

--- nestedloop.java
+++ nestedloop.java
@@ -1,26 +1,32 @@
 import java.util.Scanner;
+
 public class nestedloop {
     public static void main(String[] args) {
-Scanner sc = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
 
+        // Deklarasi array 2 dimensi
+        double[][] temps = new double[5][7];
 
-        //deklarasi array 2 dimensi
-        double [][] temps = new double[5][7];
-
-        for (int i = 0; i < temps.length; i++){
+        // Input Suhu
+        for (int i = 0; i < temps.length; i++) {
             System.out.println("kota ke-" + i);
-            for(int j = 0; j < temps[0].length;j++){
-                System.out.print("hari ke-" + (j+i) + ": ");
+            for (int j = 0; j < temps[0].length; j++) {
+                System.out.print("hari ke-" + (j + i) + ": ");
                 temps[i][j] = sc.nextDouble();
             }
             System.out.println();
         }
-        for (int i = 0; i< temps.length; i++){
+
+        // Menampilkan Nilai Rata-rata Masing-masing Kota
+        for (int i = 0; i < temps.length; i++) {
             System.out.print("kota ke-" + (i + 1) + ": ");
-            for (int j = 0; j < temps[0].length;j++){
-                System.out.print(temps [i][j] + " ");
+            double totalSuhu = 0;
+            for (int j = 0; j < temps[0].length; j++) {
+                System.out.print(temps[i][j] + " ");
+                totalSuhu += temps[i][j];
             }
-            System.out.println();
+            double rataRata = totalSuhu / temps[0].length;
+            System.out.println("- Rata-rata: " + rataRata);
         }
     }
 }
